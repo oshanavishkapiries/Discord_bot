7eb707c6420694a054aa5ef59e985bc60aed0b0b
153918928+oshanavishkapiries@users.noreply.github.com <153918928+oshanavishkapiries@users.noreply.github.com>
2025-06-09 02:23:22 +0530

    web-hook-done

--- index.js
+++ index.js
@@ -17,13 +17,12 @@ client.on("ready", () => {
 client.on("messageCreate", async (message) => {
   if (!message.author.bot) {
     try {
-      await axios.post("http://oshan-n8n-303e1f-95-111-248-142.traefik.me/webhook-test/99cae660-70b4-438b-9e74-379cc0f0faa6", {
+      await axios.post(process.env.WEB_HOOK_URL, {
         username: message.author.username,
+        authorId: message.author.id,
         content: message.content,
-        channel : message.channel,
-        channelId : message.channel.id,
-        author : message.author,
-        authorId : message.author.id,
+        channel: message.channel.name,
+        channelId: message.channel.id,
       });
     } catch (error) {
       console.error("❌ Failed to send to n8n:", error.message);
