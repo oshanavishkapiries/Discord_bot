ba0699f8d1684a24b1349f5fa56afa51ace1d157
153918928+oshanavishkapiries@users.noreply.github.com <153918928+oshanavishkapiries@users.noreply.github.com>
2025-06-09 12:09:07 +0530

    state-change

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", async (message) => {
-  if (!message.author.bot) {
+  if (!message.author.bot && message.channel.name !== "chat") {
     try {
       await axios.post(process.env.WEB_HOOK_URL, {
         username: message.author.username,
-        authorId: message.author.id,
         content: message.content,
         channel: message.channel.name,
+        authorId: message.author.id,
         channelId: message.channel.id,
+        masseageId: message.id
       });
     } catch (error) {
       console.error("❌ Failed to send to n8n:", error.message);
